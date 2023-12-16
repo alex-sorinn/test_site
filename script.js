7365f7f149de108a71737e738e863b37c5cf2721
alexsorin753 <alex.songureanu74@gmail.com>
2023-12-16 09:57:46 +0200

    random

--- script.js
+++ script.js
@@ -4,14 +4,19 @@ document.addEventListener("DOMContentLoaded", (event) => {
 });
 
 function lotto(directory, number, from_number) {
+    const highNumber = from_number + 1;
     const element = document.querySelector(directory);
     const button = element.children[1];
 
-    function random() {
-        
+    function random(min, max) {
+        let randomARR = [];
+        for(let e = 0; e < number; e++) {
+            randomARR.push(Math.floor(Math.random() * (max - min) + min));
+        }
+        return randomARR;
     }
     button.addEventListener('click', function() {
-        
+        console.log(random(number, highNumber));
     });
 
 }
