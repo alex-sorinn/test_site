caa9e2068a990c7bc7ff593cff824a32c4529e1b
alexsorin753 <alex.songureanu74@gmail.com>
2023-12-16 09:25:23 +0200

    ssh

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     console.log("loaded")
+    lotto("#lotto_649", 6, 49);
 });
+
+function lotto(directory, number, from_number) {
+    const element = document.querySelector(directory);
+    const button = element.children[1];
+
+    function random() {
+        
+    }
+    button.addEventListener('click', function() {
+        
+    });
+
+}
